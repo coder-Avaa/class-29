701bd2b2d849b108785de418fb412a3ac4795cb8
Coder-Avaa <jsh.mishra@gmail.com>
2021-05-02 12:39:54 +0530

    added modified slingshot class and sketch.js

--- Slingshot.js
+++ Slingshot.js
@@ -7,6 +7,9 @@ class SlingShot{
             length: 10
         }
         this.pointB = pointB
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
@@ -16,11 +19,25 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1,200,20);
+        image(this.sling2,170,15);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            push();
+            stroke(60,10,15);
+        if(pointA.x < 200) {
+            strokeWeight(10);
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-20,pointA.y,pointB.x+30,pointB.y-5);
+            image(this.sling3,pointA.x-30,pointA.y-10,10,30);
+        }else{
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            line(pointA.x-35, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-35,pointA.y,pointB.x+30,pointB.y-5);
+            image(this.sling3,pointA.x-30,pointA.y-10,10,30);
+        } 
+           pop();
         }
     }
     
